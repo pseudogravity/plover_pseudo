62bf76307e6acfd80374c8672c1c09701a2a6884
pseudogravity <68665732+pseudogravity@users.noreply.github.com>
2025-12-31 00:21:45 -0500

    Add files via upload

---
+++ plover_pseudo/system.py
@@ -0,0 +1,44 @@
+from plover.system import english_stenotype
+
+KEYS = (
+    "#",
+    "S-",
+    "K-",
+    "T-",
+    "P-",
+    "W-",
+    "H-",
+    "N-",
+    "R-",
+    "A-",
+    "O-",
+    "*",
+    "-E",
+    "-U",
+    "-F",
+    "-R",
+    "-P",
+    "-B",
+    "-L",
+    "-G",
+    "-T",
+    "-S",
+    "-D",
+    "-Z",
+)
+
+IMPLICIT_HYPHEN_KEYS = english_stenotype.IMPLICIT_HYPHEN_KEYS
+SUFFIX_KEYS = english_stenotype.SUFFIX_KEYS
+
+NUMBER_KEY = english_stenotype.NUMBER_KEY
+NUMBERS = english_stenotype.NUMBERS
+UNDO_STROKE_STENO = english_stenotype.UNDO_STROKE_STENO
+
+ORTHOGRAPHY_RULES = english_stenotype.ORTHOGRAPHY_RULES
+ORTHOGRAPHY_RULES_ALIASES = english_stenotype.ORTHOGRAPHY_RULES_ALIASES
+ORTHOGRAPHY_WORDLIST = english_stenotype.ORTHOGRAPHY_WORDLIST
+
+DICTIONARIES_ROOT = english_stenotype.DICTIONARIES_ROOT
+DEFAULT_DICTIONARIES = english_stenotype.DEFAULT_DICTIONARIES
+
+KEYMAPS = dict(english_stenotype.KEYMAPS)
